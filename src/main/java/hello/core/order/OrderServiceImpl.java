f2e9b4e06281eb160e8d1a2b51ce30c9b1bd2bd2
mjh9401 <mjh9401@naver.com>
2023-03-18 13:16:23 +0900

    의존관계 자동 주입

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,18 +1,21 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
+
     private final DiscountPolicy discountPolicy;
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
@@ -25,7 +28,7 @@ public class OrderServiceImpl implements OrderService{
         return new Order(memberId,itemName,itemPrice,discountPrice);
     }
 
-    //테스트 용도ㄴ
+    //테스트 용도
     public MemberRepository getMemberRepository() {
         return memberRepository;
     }
